48e52c880113197308ed837eed323ea6d8454e7f
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-04 14:00:31 +0400

    update checkbox

--- src/components/Auth/SignIn/SignIn.tsx
+++ src/components/Auth/SignIn/SignIn.tsx
@@ -139,7 +139,6 @@ export const SignIn: React.FC<SignInProps> = ({ closeSignIn }) => {
                         aria-describedby="remember"
                         type="checkbox"
                         className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-600 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-primary-300"
-                        required
                       />
                     </div>
                     <div className="ml-3 text-sm text-gray-300 text-gray-500">
